123ae54888e1fc64d1b214184cdabe2784a672ed
Cenze94 <andrea.grendene94@gmail.com>
2018-10-11 10:58:17 +0200

    Update LoadPDB

--- BiggerPython/LoadPDB.py
+++ BiggerPython/LoadPDB.py
@@ -1,3 +1,12 @@
+from enum import Enum
+
+
+"""Enumeration, I don't know what is its purpose"""
+class PDBChargeOrigin(Enum):
+    pdbNone = 1,
+    pdbOccTemp = 2
+    pdbOccupancy = 3
+    pdbCharge = 4
 
 
 class PDB:
